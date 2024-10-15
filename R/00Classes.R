aa855d4afcf6d9acaf49d960e4f49b8cf8214244
Timi Niemensivu <timinie@utu.fi>
2024-10-15 08:58:30 +0300

    Another fix still to summary
    Signed-off-by: Timi Niemensivu <timinie@utu.fi>

--- R/00Classes.R
+++ R/00Classes.R
@@ -39,10 +39,10 @@ setMethod(
                        , round(signif$ci_D[2], digits=3), sep=" - ")
     PHD_confint <- paste(round(signif$ci_PHD[1], digits=3)
                          , round(signif$ci_PHD[2], digits=3), sep=" - ")
-    ci_label <- paste("CI ", (1-call$conf.level)*100, "%(",
-                      call$alternative, ")", sep="")
+    ci_label <- paste("CI ", (1-call$conf.level)*100, "%", sep="")
     cat(sprintf("%-5s %-6s %-6s %-6s %-14s %-7s %-7s \n", " ", "Stat.", "ASE1",
-                "ASE0", ci_label, "Z-stat.", "p-value"))
+                "ASE0", ci_label, "Z-stat.", paste("p-value (",
+                                                   call$alternative, ")", sep="")))
     cat(sprintf("%-5s %-6.3f %-6.3f %-6.3f %-14s %-7.3f %-7.3f \n", "D", stats$D,
                 signif$ASE1_D, signif$ASE0_D, D_confint, signif$t_stat, signif$p_val))
     cat(sprintf("%-5s %-6.3f %-6.3f %-6.3f %-14s %-7.3f %-7.3f \n", "PHD", stats$PHD,
@@ -92,10 +92,9 @@ setMethod(
                        , round(signif$ci_G[2], digits=3), sep=" - ")
     PHG_confint <- paste(round(signif$ci_PHG[1], digits=3)
                          , round(signif$ci_PHG[2], digits=3), sep=" - ")
-    ci_label <- paste("CI ", (1-object@call$conf.level)*100, "% (",
-                      call$alternative, ")", sep="")
+    ci_label <- paste("CI ", (1-object@call$conf.level)*100, "%", sep="")
     cat(sprintf("%-5s %-6s %-6s %-14s %-7s %-7s \n", " ", "Stat.", "ASE", ci_label,
-                "Z-stat.", "p-value"))
+                "Z-stat.", paste("p-value (", call$alternative, ")", sep="")))
     cat(sprintf("%-5s %-6.3f %-6.3f %-14s %-7.3f %-7.3f \n", "G", stats$G,
                 signif$ASE_G, G_confint, signif$t_stat, signif$p_val))
     cat(sprintf("%-5s %-6.3f %-6.3f %-14s %-7.3f %-7.3f \n", "PHG", stats$PHG,
